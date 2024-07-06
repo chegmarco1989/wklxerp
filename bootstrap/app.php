38dce4dcdbf810aec3326464c07d1c25c835bda1
Shift <shift@laravelshift.com>
2024-07-06 09:51:47 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,33 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->validateCsrfTokens(except: [
+            '/install/details',
+            '/install/post-details',
+            '/install/install-alternate',
+            '/api/ecom/customers',
+            '/api/ecom/orders',
+            '/webhook/*',
+        ]);
+
+        $middleware->throttleApi();
+
+        $middleware->replace(\Illuminate\Foundation\Http\Middleware\TrimStrings::class, \App\Http\Middleware\TrimStrings::class);
+
+        $middleware->alias([
+            'AdminSidebarMenu' => \App\Http\Middleware\AdminSidebarMenu::class,
+            'CheckUserLogin' => \App\Http\Middleware\CheckUserLogin::class,
+            'EcomApi' => \App\Http\Middleware\EcomApi::class,
+            'SetSessionData' => \App\Http\Middleware\SetSessionData::class,
+            'authh' => \App\Http\Middleware\IsInstalled::class,
+            'language' => \App\Http\Middleware\Language::class,
+            'setData' => \App\Http\Middleware\IsInstalled::class,
+            'superadmin' => \App\Http\Middleware\Superadmin::class,
+            'timezone' => \App\Http\Middleware\Timezone::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
